2219928c7ed417cfe6a4239c9e0849b7433ac164
AnhTNguyenBlank <tuananhnguyen.moi@gmail.com>
2025-05-16 17:31:45 +0700

    Create new repo

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # Intraday_trading
  
-This is a general frame work for intraday trading including:
+This is a general frame work for backtesting trading strategy including:
 
-* Support functions for plotting charts, finding and visualizing supports/ resistances, finding and visualizing extremum (turning points),
-* 2 notebooks with live updating for account info (balance, equity, profit, margin, margin free, margin level) in notebook 1 and notebook 2 for live updating status for holding positions.
+* Support functions for plotting charts, finding and visualizing supports/ resistances, finding and visualizing extremum,
 
-This repo is subjected to changes in the future. Some of the future updates including: real-time risk monitoring/ alerts; general frame work for sending orders; .... This repo will play a foundation for many later projects.
+* An example notebook of how to use the classes and functions,
+
+* A base class for strategy, backtest to use in other projects.
+
+Some different features that this framework has:
+
+* In this backtesting repo, it replicates real-life trading as in we tend to increase our lot even though we only backtested with 0.01. To incorporate that, I have set up a mechanism, basically the lot will increase with the increment of 0.01 (can be modified) for every time that we double our capital (can also be modified),
+
+* In addition, there will also be a max_existing_positions parameter to ensure our margins, since trading forex, coins and gold involve a very high margins (1:100 or even 1:2000). This mechanism will be updated with the calculation of margin, free margin to better mimic real profit,
+
+* The outputs of the backtest class including dataframes with positions (df_result) and balances (df_balance), which can be used for in-sample optimization. 
--- src/models.py
+++ src/models.py
@@ -2,44 +2,12 @@ import pandas as pd
 import numpy as np
 
 pd.set_option('display.max_columns', 999)
-from scipy.stats import levy_stable
-
-from datetime import datetime
-from scipy.stats import kstest
-from scipy.stats import jarque_bera
-# from arch.unitroot import ADF
-from scipy.stats import kurtosis
-from scipy.stats import skew
-# from arch import arch_model
-
-import pickle
 
 import ta
 
 import matplotlib.pyplot as plt
 
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import webbrowser
-
-from hyperopt import tpe, hp, fmin, STATUS_OK,Trials
-from hyperopt.pyll.base import scope
-
-from scipy.signal import argrelmin
-from scipy.signal import argrelmax
-
 plt.style.use('classic')
-
-
-import MetaTrader5 as mt
-import pandas as pd
-import ml_collections
-import yaml
-
-from datetime import datetime
-import pytz
-import sys
-
 
 #=======================ALPHAS=======================
 
@@ -74,22 +42,60 @@ class RSI_strat(Base_Alpha):
         )  # Pass the strategy function to the parent constructor
         
         '''
-        config = {'RSI_param': 14, 'base_SL': 10, 'base_TP': 20}
+        config = {'RSI_PARAMS': {'INPUT_PARAM': 14, 'CUTOFF_BUY': 30, 'CUTOFF_SELL': 70}, 
+                'base_SL': 10, 
+                'base_TP': 20
+                }
         '''
-        self.RSI_param = config['RSI_param']
+        self.RSI_param = config['RSI_PARAMS']
         self.base_SL = config['base_SL']
         self.base_TP = config['base_TP']
         
         
     def _prepare_data(self, df_observe):
-        df_observe['RSI'] = ta.momentum.RSIIndicator(df_observe['CLOSE'], window = self.RSI_param).rsi()
+        df_observe['RSI'] = ta.momentum.RSIIndicator(df_observe['CLOSE'], window = self.RSI_param['INPUT_PARAM']).rsi()
         return(df_observe)
 
     def _RSI_strat(self, row):
         '''Strategy for RSI, calculates the signal based on the RSI value.'''
-        if row['RSI'] < 30:
+        if row['RSI'] < self.RSI_param['CUTOFF_BUY']:
             return pd.Series([1, self.base_SL, self.base_TP], index=['SIGNAL', 'SL', 'TP']) # Buy signal
-        elif row['RSI'] > 70:
+        elif row['RSI'] > self.RSI_param['CUTOFF_BUY']:
+            return pd.Series([-1, self.base_SL, self.base_TP], index=['SIGNAL', 'SL', 'TP']) # Sell signal
+        else:
+            return pd.Series([0, 0, 0], index=['SIGNAL', 'SL', 'TP']) # No action (neutral)
+        
+
+class MCMC_strat_v1(Base_Alpha):
+    def __init__(self, config):
+        super().__init__(
+            strat = self._MCMC_strat_v1,
+            prepare_data = self._prepare_data
+        )
+
+        '''
+        config = {
+            'base_SL': 10, 
+            'base_TP': 20,
+            'DECISION_CUTOFF': {'ENTRY_BUY_CUTOFF': 1, 'ENTRY_SELL_CUTOFF': -1}
+        }
+
+        Conduct on simple transition 1-step matrix between the previous and current log return
+        '''
+        self.base_SL = config['base_SL']
+        self.base_TP = config['base_TP']
+        self.DECISION_CUTOFF = config['DECISION_CUTOFF']
+        
+        
+    def _prepare_data(self, df_observe):
+        #returns
+        df_observe['RET(T)'] = 100*(df_observe['CLOSE'] - df_observe['CLOSE'].shift(1))/df_observe['CLOSE'].shift(1)
+        return(df_observe)
+
+    def _MCMC_strat_v1(self, row):
+        if ((row['RET(T)'] > self.DECISION_CUTOFF['ENTRY_BUY_CUTOFF']) & (pd.isnull(row['RET(T)']) == False)):
+            return pd.Series([1, self.base_SL, self.base_TP], index=['SIGNAL', 'SL', 'TP']) # Buy signal
+        elif ((row['RET(T)'] < self.DECISION_CUTOFF['ENTRY_SELL_CUTOFF']) & (pd.isnull(row['RET(T)']) == False)):
             return pd.Series([-1, self.base_SL, self.base_TP], index=['SIGNAL', 'SL', 'TP']) # Sell signal
         else:
             return pd.Series([0, 0, 0], index=['SIGNAL', 'SL', 'TP']) # No action (neutral)
